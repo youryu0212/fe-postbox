93a22b9c022fb47571e9d2ff28c35cedd52a434a
youryu0212 <youryu0212@naver.com>
2022-03-08 16:50:57 +0900

    Fix: 클래스명이 여러개인 경우 검색되지 않는 문제 해결

--- js/dom-api/getElementsByClassName.js
+++ js/dom-api/getElementsByClassName.js
@@ -2,7 +2,7 @@ export const getElementsByClassName = (node, target) => {
   const result = [];
   if (!node) return result;
   [...node.children].forEach((cur) => {
-    if (cur.className === target) {
+    if ([...cur.classList].includes(target)) {
       result.push(cur);
     }
     result.push(...getElementsByClassName(cur, target));
