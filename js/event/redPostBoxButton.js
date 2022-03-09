9b4df63a807a86643ec67451e1e55145bc7d023c
youryu0212 <youryu0212@naver.com>
2022-03-09 16:58:11 +0900

    Feat: 버튼 클릭시 마을 테두리 빨간색으로 변경

--- js/event/redPostBoxButton.js
+++ js/event/redPostBoxButton.js
@@ -1,28 +1,41 @@
 import { getElementsByClassName, getElementByClassName } from ".././dom-api/index.js";
 import { mergeSort } from "../util.js";
 
-const searchRedPostbox = () => {
-  const villagesHasRedPostbox = getElementsByClassName(document, "has-red-postbox");
-  return villagesHasRedPostbox;
-};
-const getVillageName = (village) => {
-  const villageName = getElementByClassName(village, "village-name").innerText;
-  return villageName;
-};
-const getRedPostboxSize = (village) => {
-  const redPostboxSize = Number(getElementByClassName(village, "red-postbox").dataset.size);
-  return redPostboxSize;
-};
+const searchVillagesRedPostbox = () => getElementsByClassName(document, "has-red-postbox");
+const getVillageName = (village) => getElementByClassName(village, "village-name").innerText;
+const getRedPostboxSize = (village) => Number(getElementByClassName(village, "red-postbox").dataset.size);
+
 const redPostboxButton = getElementByClassName(document, "red-postbox__button");
 
-redPostboxButton.addEventListener("click", () => {
-  const villages = searchRedPostbox();
-  const postboxSize = villages.map((village) => [getVillageName(village), getRedPostboxSize(village)]);
+const renderRedPostboxInfo = (villagesHasRedPostbox) => {
   const redPostboxInfo = getElementByClassName(document, "red-postbox__info");
   redPostboxInfo.innerHTML = `
-  <div>${postboxSize.map(([name, _]) => name)} 총 ${postboxSize.length} 개의 마을입니다.</div>
-  <div>우체통의 크기는${mergeSort(postboxSize, (a, b) => a[1] >= b[1]).map(([name, _]) => name)}입니다.</div>
+  <div>
+    ${villagesHasRedPostbox.map((village) => village.name)} 총 ${
+    villagesHasRedPostbox.length
+  } 개의 마을입니다.
+  </div>
+  <div>
+  우체통의 크기는${mergeSort(villagesHasRedPostbox, (a, b) => a.postboxSize >= b.postboxSize).map(
+    (village) => village.name
+  )}입니다.
+  </div>
   `;
+};
+redPostboxButton.addEventListener("click", () => {
+  new Promise((resolve, reject) => {
+    const villageHasRedPostboxElements = searchVillagesRedPostbox();
+    const villagesHasRedPostbox = villageHasRedPostboxElements.map((villageElement) => {
+      return { name: getVillageName(villageElement), postboxSize: getRedPostboxSize(villageElement) };
+    });
+    renderRedPostboxInfo(villagesHasRedPostbox);
+    setTimeout(() => resolve(villageHasRedPostboxElements), 2000);
+  }).then((villageHasRedPostboxElements) => {
+    villageHasRedPostboxElements.forEach((village) => {
+      const villageArea = getElementByClassName(village, "village-area");
+      villageArea.style.borderColor = "red";
+    });
+  });
 });
 
 export { redPostboxButton };
