8bfd70dee6fba5ff6604e470ca7fda003cdbb2d4
youryu0212 <youryu0212@naver.com>
2022-03-10 21:20:53 +0900

    Feat: 마을 클래스 생성

--- js/village.js
+++ js/village.js
@@ -1,18 +1,28 @@
+import { getRandomNumber } from "./util.js";
+
 export class Village {
-  constructor(name, { x, y }, size, postboxSize = null) {
+  constructor(name, { x, y, w, h }, hasPostbox = false) {
     this.name = name;
-    [this.x, this.y] = [x, y];
-    this.size = size;
-    this.postboxSize = postboxSize;
-    this.hasPostbox = !!postboxSize;
+    [this.x, this.y, this.w, this.h] = [x, y, w, h];
+    this.postboxSize = hasPostbox ? getRandomNumber(1, 10) : null;
+    this.hasPostbox = hasPostbox;
     this.innerVillage = [];
   }
   createTemplate() {
     return `
-    <div class="village${this.hasPostbox ? " has-red-postbox" : ""}">
+    <div class="village${this.hasPostbox ? " has-red-postbox" : ""}" style="${`
+      top:${this.y}px;
+      left:${this.x}px;
+      width:${this.w}px;
+      height:${this.h}px;
+      `}">
       <div class="village-name">${this.name}</div>
       <div class="village-area">
-        ${this.hasPostbox ? `<div class="red-postbox" data-size="${this.postboxSize}">📮</div>` : ""}
+        ${
+          this.hasPostbox
+            ? `<div class="red-postbox" data-size="${this.postboxSize}">📮${this.postboxSize}</div>`
+            : ""
+        }
         ${this.innerVillage.map((village) => village.createTemplate()).join("")}
       </div>
     </div>
