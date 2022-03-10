c1584a94035e68d5a7b5d1ff538fe8c020833aa7
youryu0212 <youryu0212@naver.com>
2022-03-10 21:21:40 +0900

    Refactor: html 하드코딩된 마을 동적으로 생성

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 import { redPostboxButton } from "./event/redPostBoxButton.js";
+import { createVillagePosition } from "./postbox.js";
+import { getRandomNumber, gacha } from "./util.js";
+import { Village } from "./village.js";
+
+const positionToVillage = (pos) => new Village(String.fromCharCode(nameCode++), pos, gacha(50));
+
+const p = createVillagePosition(1500, 700);
+let nameCode = 65;
+const villages = p.map(positionToVillage);
+villages.forEach((village) => {
+  village.innerVillage = createVillagePosition(village.w, village.h).map(positionToVillage);
+});
+
+document.querySelector(".map").innerHTML = villages.map((village) => village.createTemplate()).join("");
