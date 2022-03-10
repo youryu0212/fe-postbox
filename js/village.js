76ff60e42049bda8dde400049387c14052300d7d
youryu0212 <youryu0212@naver.com>
2022-03-10 16:18:08 +0900

    Feat: 마을 객체 Create Template 함수 생성

--- js/village.js
+++ js/village.js
@@ -1,7 +1,21 @@
 export class Village {
-  constructor({ x, y }, size, color) {
+  constructor(name, { x, y }, size, postboxSize = null) {
+    this.name = name;
     [this.x, this.y] = [x, y];
     this.size = size;
-    this.color = color;
+    this.postboxSize = postboxSize;
+    this.hasPostbox = !!postboxSize;
+    this.innerVillage = [];
+  }
+  createTemplate() {
+    return `
+    <div class="village${this.hasPostbox ? " has-red-postbox" : ""}">
+      <div class="village-name">${this.name}</div>
+      <div class="village-area">
+        ${this.hasPostbox ? `<div class="red-postbox" data-size="${this.postboxSize}">📮</div>` : ""}
+        ${this.innerVillage.map((village) => village.createTemplate()).join("")}
+      </div>
+    </div>
+    `;
   }
 }
