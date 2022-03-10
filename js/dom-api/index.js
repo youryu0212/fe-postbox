b2c3048346fbb900f4c26f6ae8519efcf34a666f
youryu0212 <youryu0212@naver.com>
2022-03-10 14:43:34 +0900

    Rename: dom-api 파일들 하나로 병합

--- js/dom-api/index.js
+++ js/dom-api/index.js
@@ -1,4 +1,25 @@
-import { getElementsByClassName } from "./getElementsByClassName.js";
-import { getElementByClassName } from "./getElementByClassName.js";
+const getElementByClassName = (node, target) => {
+  let result = null;
+  if (!node) return null;
+  for (let i = 0; i < node.children.length; i++) {
+    if ([...node.children[i].classList].includes(target)) {
+      return node.children[i];
+    }
+    result = getElementByClassName(node.children[i], target);
+    if (result) return result;
+  }
+};
+
+const getElementsByClassName = (node, target) => {
+  const result = [];
+  if (!node) return result;
+  [...node.children].forEach((cur) => {
+    if ([...cur.classList].includes(target)) {
+      result.push(cur);
+    }
+    result.push(...getElementsByClassName(cur, target));
+  });
+  return result;
+};
 
 export { getElementsByClassName, getElementByClassName };
