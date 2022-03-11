aedc6ea6e0bf785781e8d474810502a9c591f4b1
youryu0212 <youryu0212@naver.com>
2022-03-11 15:07:53 +0900

    Feat: 깊이를 입력받아 마을의 중첩 구조 생성 기능 구현

--- server/data.js
+++ server/data.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const data = express.Router();
-
+const MAX_WIDTH = 1500;
+const MAX_HEIGHT = 700;
+const DEPTH = 4;
 const { createVillagePosition } = require("./createVillagePosition.js");
 const { gacha } = require("./util.js");
 const { Village } = require("./village.js");
 
-const createRandomVillages = () => {
+const createRandomVillagesGenerator = () => {
   const positionToVillage = (pos) => new Village(String.fromCharCode(nameCode++), pos, gacha(50));
-
-  const villagePosition = createVillagePosition(1500, 700);
   let nameCode = 65;
-  const villages = villagePosition.map(positionToVillage);
-  villages.forEach((village) => {
-    village.innerVillage = createVillagePosition(village.w, village.h).map(positionToVillage);
-  });
-  return villages;
+  const createRandomVillages = (maxWidth, maxHeight, depth) => {
+    if (depth === 0) return [];
+
+    const villagePosition = createVillagePosition(maxWidth, maxHeight);
+    const villages = villagePosition.map(positionToVillage);
+    villages.forEach((village) => {
+      village.innerVillage = createRandomVillages(village.w, village.h, depth - 1);
+    });
+    return villages;
+  };
+  return createRandomVillages;
 };
-const createVillageTemplate = (villages) => villages.map((village) => village.createTemplate()).join("");
 
 data.get("/", (req, res) => {
-  const villages = createRandomVillages();
+  const createVillageTemplate = (villages) => villages.map((village) => village.createTemplate()).join("");
+  const createRandomVillages = createRandomVillagesGenerator();
+  const villages = createRandomVillages(MAX_WIDTH, MAX_HEIGHT, DEPTH);
   const villagesTemplate = createVillageTemplate(villages);
   res.json(JSON.stringify(villagesTemplate));
 });
