c016bee0760592c32ff936e4716e791f3bb0b0f1
youryu0212 <youryu0212@naver.com>
2022-03-11 11:42:16 +0900

    Refactor: Server data.js 함수 분리

--- server/data.js
+++ server/data.js
@@ -5,16 +5,22 @@ const { createVillagePosition } = require("./createVillagePosition.js");
 const { gacha } = require("./util.js");
 const { Village } = require("./village.js");
 
-data.get("/", (req, res) => {
+const createRandomVillages = () => {
   const positionToVillage = (pos) => new Village(String.fromCharCode(nameCode++), pos, gacha(50));
 
-  const p = createVillagePosition(1500, 700);
+  const villagePosition = createVillagePosition(1500, 700);
   let nameCode = 65;
-  const villages = p.map(positionToVillage);
+  const villages = villagePosition.map(positionToVillage);
   villages.forEach((village) => {
     village.innerVillage = createVillagePosition(village.w, village.h).map(positionToVillage);
   });
-  const villagesTemplate = villages.map((village) => village.createTemplate()).join("");
+  return villages;
+};
+const createVillageTemplate = (villages) => villages.map((village) => village.createTemplate()).join("");
+
+data.get("/", (req, res) => {
+  const villages = createRandomVillages();
+  const villagesTemplate = createVillageTemplate(villages);
   res.json(JSON.stringify(villagesTemplate));
 });
 
